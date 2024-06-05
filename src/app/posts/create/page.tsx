bd727babab0ee6b1e13a0ec8ca54c501183df93a
meirosa <164320531+meirosa@users.noreply.github.com>
2024-06-05 15:50:04 +0700

    Update page.tsx

--- src/app/posts/create/page.tsx
+++ src/app/posts/create/page.tsx
@@ -1,24 +1,11 @@
-import { updatePost } from "@/app/actions/posts";
+import { createPost } from "@/app/actions/posts";
 import PostForm from "@/components/post-form";
-import { fetchPostById } from "@/db/queries/posts";
 
-interface PostsEditProps {
-    params: {
-        id: string;
-    };
-}
-
-export default async function PostsEdit({ params }: PostsEditProps) {
-    const { id } = params;
-
-    const post = await fetchPostById(id)
-
-    const updateAction = updatePost.bind(null, id)
-
+export default function PostsCreate() {
     return (
-        <main className="min-h-screen flex items-center justify-center bg-white dark:bg-gray-800 p-8">
-            <div className="w-full max-w-md mx-auto bg-gray-100 dark:bg-gray-900 p-6 rounded-lg shadow-md">
-                <PostForm formAction={updateAction} initialData={{ title: post?.title ?? '', content: post?.content ?? '' }} />
+        <main className="min-h-screen flex items-center justify-center bg-white p-8">
+            <div className="w-full max-w-md mx-auto bg-blue-400 p-6 rounded-lg shadow-md">
+                <PostForm formAction={createPost} initialData={{ title: '', content: '' }} />
             </div>
         </main>
     );
