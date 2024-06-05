45850b5b1add3a12741dd83c703debbf1524d708
meirosa <164320531+meirosa@users.noreply.github.com>
2024-06-05 15:52:23 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import PostDelete from "@/components/post-delete";
 
 export default async function Home() {
-  const posts = await fetchPosts()
+  const posts = await fetchPosts();
   const dateOptions: Intl.DateTimeFormatOptions = {
     year: 'numeric',
     month: 'long',
@@ -13,28 +13,30 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-start p-24">
       <div className="mb-4">
-        <Link href="/posts/create" className="bg-white px-4 py-2 rounded">Create Post</Link>
+        <Link href="/posts/create" className="bg-blue-400 px-4 py-2 rounded border border-blue-900 text-white font-bold">Create Post</Link>
       </div>
       <div className="mb-32 grid gap-x-8 gap-y-4 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         {posts.map(post => {
-          return <div key={post.id}>
-            <div className="mb-4">
-              <Link
-                key={post.id}
-                href={`/posts/${post.id}/edit`}
-                className=""
-              >
-                <h2 className={`mb-3 text-2xl font-semibold`}>
-                  {post.title}
-                </h2>
-              </Link>
-              <p className={`m-0 max-w-[30ch] text-sm opacity-60`}>
-                {post.content}
-              </p>
+          return (
+            <div key={post.id} className="border border-gray-400 rounded p-4 border-black">
+              <div className="mb-4">
+                <Link
+                  key={post.id}
+                  href={`/posts/${post.id}/edit`}
+                  className=""
+                >
+                  <h2 className={`mb-3 text-2xl font-semibold`}>
+                    {post.title}
+                  </h2>
+                </Link>
+                <p className={`m-0 max-w-[30ch] text-sm opacity-60`}>
+                  {post.content}
+                </p>
+              </div>
+              <div className="text-sm opacity-30">{'Updated at ' + post.updatedAt.toLocaleDateString("en-US", dateOptions)}</div>
+              <PostDelete id={post.id} />
             </div>
-            <div className="text-sm opacity-30">{'Updated at ' + post.updatedAt.toLocaleDateString("en-US", dateOptions)}</div>
-            <PostDelete id={post.id} />
-          </div>
+          );
         })}
       </div>
     </main>
