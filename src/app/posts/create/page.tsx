00e86014b01b047a53dc114a755cb6bc289bc73b
meirosa <164320531+meirosa@users.noreply.github.com>
2024-06-12 12:24:40 +0700

    Update page.tsx

--- src/app/posts/create/page.tsx
+++ src/app/posts/create/page.tsx
@@ -3,8 +3,8 @@ import PostForm from "@/components/post-form";
 
 export default function PostsCreate() {
     return (
-        <main className="min-h-screen flex items-center justify-center bg-white p-8">
-            <div className="w-full max-w-md mx-auto bg-blue-400 p-6 rounded-lg shadow-md">
+        <main className="min-h-screen flex items-center justify-center bg-[#99dbe4] p-8">
+            <div className="w-full max-w-md mx-auto bg-white p-6 rounded-lg shadow-md">
                 <PostForm formAction={createPost} initialData={{ title: '', content: '' }} />
             </div>
         </main>
