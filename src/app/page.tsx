456836b9043ec25a99a8059b5ecad274ccf5cd18
meirosa <164320531+meirosa@users.noreply.github.com>
2024-06-12 12:25:15 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
       <div className="mb-32 grid gap-x-8 gap-y-4 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         {posts.map(post => {
           return (
-            <div key={post.id} className="border border-gray-400 rounded p-4 border-black">
+            <div key={post.id} className="border border-gray-400 rounded p-4 border-black bg-white">
               <div className="mb-4">
                 <Link
                   key={post.id}
@@ -29,11 +29,11 @@ export default async function Home() {
                     {post.title}
                   </h2>
                 </Link>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-60`}>
+                <p className={`m-0 max-w-[30ch] text-sm opacity-80`}>
                   {post.content}
                 </p>
               </div>
-              <div className="text-sm opacity-30">{'Updated at ' + post.updatedAt.toLocaleDateString("en-US", dateOptions)}</div>
+              <div className="text-sm opacity-50">{'Updated at ' + post.updatedAt.toLocaleDateString("en-US", dateOptions)}</div>
               <PostDelete id={post.id} />
             </div>
           );
