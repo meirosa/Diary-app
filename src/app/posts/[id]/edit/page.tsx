90e24e23acc5b6c038c32cd330e035ddffffbf8b
meirosa <164320531+meirosa@users.noreply.github.com>
2024-06-12 12:24:21 +0700

    Update page.tsx

--- src/app/posts/[id]/edit/page.tsx
+++ src/app/posts/[id]/edit/page.tsx
@@ -11,13 +11,13 @@ interface PostsEditProps {
 export default async function PostsEdit({ params }: PostsEditProps) {
     const { id } = params;
 
-    const post = await fetchPostById(id)
+    const post = await fetchPostById(id);
 
-    const updateAction = updatePost.bind(null, id)
+    const updateAction = updatePost.bind(null, id);
 
     return (
-        <main className="min-h-screen flex items-center justify-center bg-white dark:bg-gray-800 p-8">
-            <div className="w-full max-w-md mx-auto bg-gray-100 dark:bg-gray-900 p-6 rounded-lg shadow-md">
+        <main className="min-h-screen flex items-center justify-center" style={{ backgroundColor: '#99dbe4' }}>
+            <div className="w-full max-w-md mx-auto bg-white dark:bg-gray-900 p-6 rounded-lg shadow-md">
                 <PostForm formAction={updateAction} initialData={{ title: post?.title ?? '', content: post?.content ?? '' }} />
             </div>
         </main>
